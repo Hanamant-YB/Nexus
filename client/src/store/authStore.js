9b1a9a18f7f5dfee2799cfbb90c0580fe3b1c70e
Hanamant-YB <hbhajantri516@gmail.com>
2026-07-17 12:23:51 +0530

    created authstore

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -1,1 +1,10 @@
 import {create} from "zustand";
+
+const useAuthStore = create((set)=>({
+    //readd the from localstorage on startup
+    //so the user stays logged in after refersh
+    token:localStorage.getItem("nexus_token")||null,
+    user:JSON.parse(localStorage.getItem("nexus_user"))||null,
+
+    //called
+}));
