8edad9f979aee83b8ebf3d732a53ba0c4606bede
Hanamant-YB <hbhajantri516@gmail.com>
2026-07-17 16:19:12 +0530

    created the protectedroute

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,1 +1,15 @@
 import{Navigate} from "react-router-dom";
+import userAuthStore from "../store/authStore";
+import { Children } from "react";
+
+const ProtectedRoute = ({Children})=>{
+    const {token} = userAuthStore();
+    //if not token = not loged in = redirect to login
+    if(!token){
+        return <Navigate to="/login" replace/>
+    }
+    //token exists = render the actual page
+    return Children;
+};
+
+export default ProtectedRoute;
